872878892420dccfd8a1e394b6dc5aef112d571d
scyc92 <scyc.92@gmail.com>
2023-03-20 22:07:08 -0300

    Histogramas

---
+++ 1. Big Data Analytics com R e Microsoft Azure Machine Learning/03. Graficos/04-Histogramas.R
@@ -0,0 +1,44 @@
+# Histogramas 
+
+# Obs: Caso tenha problemas com a acentuação, consulte este link:
+# https://support.rstudio.com/hc/en-us/articles/200532197-Character-Encoding
+
+# Configurando o diretório de trabalho
+# Coloque entre aspas o diretório de trabalho que você está usando no seu computador
+# Não use diretórios com espaço no nome
+setwd("C:/FCD/BigDataRAzure/Cap04")
+getwd()
+
+# Definindo os dados
+?cars
+View(cars)
+dados = cars$speed
+
+# Construindo um histograma
+?hist
+hist(dados)
+
+
+# Conforme consta no help, o parâmetro breaks pode ser um dos itens abaixo:
+# Um vetor para os pontos de quebra entre as células do histograma
+# Uma função para calcular o vetor de breakpoints
+# Um único número que representa o número de células para o histograma
+# Uma cadeia de caracteres que nomeia um algoritmo para calcular o número de células 
+# Uma função para calcular o número de células.
+hist(dados, breaks = 10, main = "Histograma das Velocidades")
+hist(dados, labels = T, breaks = c(0,5,10,20,30), main = "Histograma das Velocidades")
+hist(dados, labels = T, breaks = 10, main = "Histograma das Velocidades")
+hist(dados, labels = T, ylim = c(0,10), breaks = 10, main = "Histograma das Velocidades")
+
+
+# Adicionando linhas ao histograma
+grafico <- hist(dados, breaks = 10, main = "Histograma das Velocidades")
+
+xaxis = seq(min(dados), max(dados), length = 10)
+yaxis = dnorm(xaxis, mean = mean(dados), sd = sd(dados))
+yaxis = yaxis*diff(grafico$mids)*length(dados)
+
+lines(xaxis, yaxis, col = "red")
+
+
+
