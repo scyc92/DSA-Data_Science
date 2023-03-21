7e2df543f86d72774c039bade04eaa25fe87d70c
scyc92 <scyc.92@gmail.com>
2023-03-20 22:06:45 -0300

    ScatterPlot

---
+++ 1. Big Data Analytics com R e Microsoft Azure Machine Learning/03. Graficos/02-ScatterPlot.R
@@ -0,0 +1,38 @@
+# Scatterplots 
+
+# Obs: Caso tenha problemas com a acentuação, consulte este link:
+# https://support.rstudio.com/hc/en-us/articles/200532197-Character-Encoding
+
+# Configurando o diretório de trabalho
+# Coloque entre aspas o diretório de trabalho que você está usando no seu computador
+# Não use diretórios com espaço no nome
+setwd("C:/FCD/BigDataRAzure/Cap04")
+getwd()
+
+# Define os dados
+set.seed(67)
+x = rnorm(10,5,7)
+y = rpois(10,7)
+z = rnorm(10,6,7)
+t = rpois(10,9)
+
+
+# Cria o Plot
+plot(x, y, col = 123, pch = 10, main = "Multi Scatterplot",
+     col.main = "red", cex.main = 1.5, xlab = "Variável Independente", 
+     ylab = "Variável Dependente")
+
+# Adiciona outros dados 
+points(z, t, col = "blue", pch = 4)
+
+# Adiciona outros dados 
+points(y, t, col = 777, pch = 9)
+
+# Cria legenda
+legend(-6,5.9, legend = c("Nível 1", "Nível 2", "Nível 3"),
+       col = c(123, "blue", 777), pch = c(10,4,9),
+       cex = 0.65, bty = "n")
+
+
+
+
