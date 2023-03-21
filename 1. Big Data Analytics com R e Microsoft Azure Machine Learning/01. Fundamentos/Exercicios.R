77024d2365a5775448bf4e6b81f5ac30b57ae825
scyc92 <scyc.92@gmail.com>
2023-03-20 21:27:35 -0300

    Exercicio & Solução

---
+++ 1. Big Data Analytics com R e Microsoft Azure Machine Learning/01. Fundamentos/Exercicios.R
@@ -0,0 +1,80 @@
+# Lista de Exercícios
+
+# Obs: Caso tenha problemas com a acentuação, consulte este link:
+# https://support.rstudio.com/hc/en-us/articles/200532197-Character-Encoding
+
+# Configurando o diretório de trabalho
+# Coloque entre aspas o diretório de trabalho que você está usando no seu computador
+# Não use diretórios com espaço no nome
+setwd("C:/FCD/BigDataRAzure/Cap02")
+getwd()
+
+# Exercício 1 - Crie um vetor com 12 números inteiros
+vec <- c(1:12)
+vec
+
+
+# Exercício 2 - Crie uma matriz com 4 linhas e 4 colunas preenchida com números inteiros
+mat <- matrix(c(1:16), nrow = 4, ncol = 4)
+mat
+
+
+# Exercício 3 - Crie uma lista unindo o vetor e matriz criados anteriormente
+lst <- list(vec, mat)
+lst
+
+
+# Exercício 4 - Usando a função read.table() leia o arquivo do link abaixo para uma dataframe
+# http://data.princeton.edu/wws509/datasets/effort.dat
+df <- data.frame(read.table("http://data.princeton.edu/wws509/datasets/effort.dat"))
+class(df)
+df
+View(df)
+
+
+# Exercício 5 - Transforme o dataframe anterior, em um dataframe nomeado com os seguintes labels:
+# c("config", "esfc", "chang")
+names(df) = c("config", "esfc", "chang")
+names(df) = c("Col1", "Col2", "Col3")
+df
+
+
+# Exercício 6 - Imprima na tela o dataframe iris, 
+# verifique quantas dimensões existem no dataframe iris, imprima um resumo do dataset
+iris
+class(iris)
+dim(iris)
+summary(iris)
+str(iris)
+View(iris)
+
+
+# Exercício 7 - Crie um plot simples usando as duas primeiras colunas do dataframe iris
+plot(iris$Sepal.Length, iris$Sepal.Width)
+
+
+# Exercício 8 - Usando s função subset, crie um novo dataframe com o conjunto de dados do dataframe iris em que Sepal.Length > 7
+# Dica: consulte o help para aprender como usar a função subset()
+?subset
+iris1 <- subset(iris, Sepal.Length > 7)
+View(iris1)
+
+
+# Exercícios 9 (Desafio) - Crie um dataframe que seja cópia do dataframe iris e usando a função slice(), 
+# divida o dataframe em um subset de 15 linhas
+# Dica 1: você vai ter que instalar e carregar o pacote dplyr
+# Dica 2: Consulte o help para aprender como usar a função slice()
+novo_iris <- iris
+novo_iris
+install.packages("dplyr")
+library(dplyr)
+?slice
+slice(novo_iris, 1:15)
+class(slice(novo_iris, 1:15))
+
+
+# Exercícios 10 - Use a função filter no seu novo dataframe criado no item anterior 
+# e retorne apenas valores em que Sepal.Length > 6
+# Dica: Use o RSiteSearch para aprender como usar a função filter
+RSiteSearch('filter')
+filter(novo_iris, Sepal.Length > 7)
