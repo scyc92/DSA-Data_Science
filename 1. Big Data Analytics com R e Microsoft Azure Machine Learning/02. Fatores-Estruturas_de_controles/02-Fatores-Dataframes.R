8d457efd45fd29b16f1d959d46ba1edc9074ec06
scyc92 <scyc.92@gmail.com>
2023-03-20 22:02:48 -0300

    Dataframes

---
+++ 1. Big Data Analytics com R e Microsoft Azure Machine Learning/02. Fatores-Estruturas_de_controles/02-Fatores-Dataframes.R
@@ -0,0 +1,65 @@
+# Fatores e Dataframes - Compreendendo a Ordem dos Fatores
+
+# Obs: Caso tenha problemas com a acentuação, consulte este link:
+# https://support.rstudio.com/hc/en-us/articles/200532197-Character-Encoding
+
+# Configurando o diretório de trabalho
+# Coloque entre aspas o diretório de trabalho que você está usando no seu computador
+# Não use diretórios com espaço no nome
+setwd("C:/FCD/BigDataRAzure/Cap03")
+getwd()
+
+# Níveis dos fatores
+# Internamente, o R armazena valores inteiros e faz um mapeamento para as strings (em ordem alfabética)
+# e agrupa as estatísticas por níveis.
+
+# Criando vetores
+vec1 <- c(1001, 1002, 1003, 1004, 1005)
+vec2 <- c(0, 1, 1, 0, 2)
+vec3 <- c('Verde','Laranja','Azul','Laranja','Verde')
+
+# Unindo os vetores em um dataframe
+df <- data.frame(vec1, vec2, vec3)
+df
+
+# Verificando que o R categorizou a última coluna como fator
+str(df)
+
+# Verificando os níveis do fator. Perceba que os níveis estão categorizados em ordem alfabética
+levels(df$vec3)
+
+# Criando uma outra coluna e atribuindo labels
+df$cat1 <- factor(df$vec3, labels = c("cor2", "cor1", "cor3"))
+df
+
+# Internamente, os fatores são registrados como inteiros, mas a ordenação segue a ordem alfabética
+# das strings
+str(df)
+
+# Veja como foi feita a atribuição:
+# Azul = cor2
+# Laranja = cor1
+# Verde = cor3
+# Ou seja, os vetores com os labels, seguiram a ordem alfabética dos níveis classificados pelo R
+
+# Criando uma outra coluna e atribuindo labels
+# Ao aplicarmos a função factor() a coluna vec2, internamente o R classificou em ordem alfabética
+# e quando atribuímos os labels, foi feita a associação.
+df$cat2 <- factor(df$vec2, labels = c("Divorciado", "Casado", "Solteiro"))
+df
+str(df)
+levels(df$cat2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
